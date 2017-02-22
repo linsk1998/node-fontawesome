f7aeb0bba98d6a88876ab858567b8b37398c26ee
drnextgis <rykovd@gmail.com>
2017-02-22 17:47:19 +0700

    Fix regexp to parse more icons

--- generate.js
+++ generate.js
@@ -17,10 +17,10 @@ require("http").get({
         var fa = "var fa=function(i){return fa[i.replace(/-./g,function(x){return x.substr(1).toUpperCase()})]};"
 
         var namecount = 0;
-        var main = body.match(/(\}\.fa-[a-z\-]+(?::before,\.fa-[a-z\-]+)*):before\{content:"\\([0-9a-f]+)"/g);
+        var main = body.match(/(\}\.fa-[a-z0-9\-]+(?::before,\.fa-[a-z0-9\-]+)*):before\{content:"\\([0-9a-f]+)"/g);
         if (!main) { console.log("Couldn't parse icons"); return process.exit(1); }
         for (var i = 0; i < main.length; i++) {
-            var icon = main[i].match(/(\}\.fa-[a-z\-]+(?::before,\.fa-[a-z\-]+)*):before\{content:"\\([0-9a-f]+)"/).slice(1);
+            var icon = main[i].match(/(\}\.fa-[a-z0-9\-]+(?::before,\.fa-[a-z0-9\-]+)*):before\{content:"\\([0-9a-f]+)"/).slice(1);
             icon[0] = icon[0].substr(5).split(/:before,\.fa-/);
             for (var k = 0; k < icon[0].length; k++) {
                 icon[0][k] = icon[0][k].replace(/-./g, function(x) { return x.substr(1).toUpperCase() });
